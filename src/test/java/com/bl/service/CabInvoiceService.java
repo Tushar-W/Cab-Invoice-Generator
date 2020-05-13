1b024f43754c536b56c4d98cdcdec8589f84c933
Tushar-W <twankhede95@gmail.com>
2020-05-14 00:14:28 +0530

    ADDED:code to create functionality to calculate total fare of multiple rides

--- src/test/java/com/bl/service/CabInvoiceService.java
+++ src/test/java/com/bl/service/CabInvoiceService.java
@@ -13,4 +13,12 @@ public class CabInvoiceService {
             return MINIMUM_FARE;
         return totalFare;
     }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride:rides) {
+             totalFare += this.calculateFare(ride.distance,ride.time);
+        }
+        return totalFare;
+    }
 }
